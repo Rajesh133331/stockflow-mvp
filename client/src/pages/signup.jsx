d9d8109e16f9287d94f5408bbe4a51665a3662b7
Paruchuri Rajesh <paruchurirajesh133331@gmail.com>
2026-07-02 11:19:30 +0530

    Implement authentication pages and configure routing

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Signup.css";
+import "../styles/Signup.css";
 
 const Signup = () => {
   return (
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -25,3 +25,4 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
+
