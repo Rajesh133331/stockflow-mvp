c88fb468fa156d04497cb5fe6d0c783a5ee50fbf
Paruchuri Rajesh <paruchurirajesh133331@gmail.com>
2026-07-02 21:52:14 +0530

    Implementation of login api  and user authentication with jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { Organization, User } from "../models/index.js";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -38,6 +39,54 @@ export const signup = async (req, res) => {
     res.status(201).json({
       message: "User registered successfully.",
       user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Internal server error.",
+    });
+  }
+};
+
+
+//login api
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and Password are required.",
+      });
+    }
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found.",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid password.",
+      });
+    }
+
+    const token = generateToken(user.id);
+
+    res.status(200).json({
+      message: "Login successful.",
+      token,
     });
   } catch (error) {
     console.log(error);
