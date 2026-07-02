ecc79d4b2058bb307f138c85b3937a370fe821cb
Paruchuri Rajesh <paruchurirajesh133331@gmail.com>
2026-07-02 21:22:48 +0530

    Implementation of user signup API

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { sequelize } from "./models/index.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -11,26 +12,16 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("StockFlow Backend Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
-sequelize
-  .authenticate()
-  .then(async () => {
-    console.log("Database connected successfully.");
 
-    await sequelize.sync({ alter: true });
 
-    console.log("Database synchronized successfully.");
-
-    app.listen(PORT, () => {
+app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((error) => {
-    console.log("Database connection failed.");
-    console.error(error);
-  });
---
+++ server/controllers/authController.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import { Organization, User } from "../models/index.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { organizationName, email, password } = req.body;
+
+    if (!organizationName || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required.",
+      });
+    }
+
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists.",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const organization = await Organization.create({
+      organizationName,
+    });
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      organizationId: organization.id,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully.",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Internal server error.",
+    });
+  }
+};
